6f606fbfe635f8b625bdef5eb63e6bcb30a24c7b
Vitor Sousa <vitormrcsousa@hotmail.com>
2026-06-05 14:32:49 +0100

    Initial Portugal Textile Hub site

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <iframe
+      src="/site.html"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: "none",
+      }}
+    />
+  );
+}
